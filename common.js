d5548c5bf44ec1a5ae07e862fe9983b81b1098ad
Ivan Ayala <network.9961@gmail.com>
2023-08-22 23:43:24 -0500

    FULL CV

--- common.js
+++ common.js
@@ -5,6 +5,6 @@ document.addEventListener("DOMContentLoaded", function() {
 function GetLastUpdateDate() {
     const months = [ "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre" ];
     const lastMoifiedDate = new Date(document.lastModified);
-    const headerString = `Ultimos cambios (ultima modificacion: ${lastMoifiedDate.getDay()} / ${months[lastMoifiedDate.getMonth()]} / ${lastMoifiedDate.getFullYear()}`;
+    const headerString = `Ultimos cambios (ultima modificacion: 22 / Agosto / 2023)`;
     document.getElementById("updates-table-header").innerHTML = headerString;
 }
